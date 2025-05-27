2533476855ee205f6d1173a85c1d37eb65915db7
flynn.lucas <flynn.lucas@T605-10>
2025-05-28 10:14:27 +1200

    Add project files.

---
+++ EndOfYearProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EndOfYearProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            string password;
+            Console.WriteLine("--------------------------Welcometo the Bus App--------------------------------------");
+            Console.WriteLine("This app will let you search for bus routes and get Prices for those respected routes");
+            Console.WriteLine("Enter your username");
+            userName = Console.ReadLine();
+            Console.WriteLine("Enter your password");
+            password = Console.ReadLine();
+            
+            if (userName = true)
+            {
+                Console.WriteLine("Welcome admin!");
+            }
+            else
+            {
+                Console.WriteLine("Welcome user!");
+            }
+
+        }
+    }
+}
