6b98bdb0ba979a1f8ae89164f4f54d0847cf8d25
Flynn Lucas <Flynn.Lucas@soit.local>
2025-05-28 10:54:00 +1200

    your message here

--- EndOfYearProject/Program.cs
+++ EndOfYearProject/Program.cs
@@ -18,7 +18,7 @@ namespace EndOfYearProject
             userName = Console.ReadLine();
             Console.WriteLine("Enter your password");
             password = Console.ReadLine();
-            Console.WriteLine("Wow");
+            Console.WriteLine("flynntest");
             if (userName = true)
             {
                 Console.WriteLine("Welcome admin!");
