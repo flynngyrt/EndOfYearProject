a0dadd77acbb4c0bc46f0a3eabd0839253d455f5
flynngyrt <flynnlucas99@gmail.com>
2025-05-28 12:04:16 +1200

    gith

--- EndOfYearProject/Program.cs
+++ EndOfYearProject/Program.cs
@@ -11,8 +11,8 @@ namespace EndOfYearProject
         static void Main(string[] args)
         {
             string[] userName = { "admin", "user" };
-            string[] passWord= { "adminpass", "userpass" };
-            int i;
+            string[] passWord = { "adminpass", "userpass" };
+            int[] i;
             Console.WriteLine("--------------------------Welcometo the Bus App--------------------------------------");
             Console.WriteLine("This app will let you search for bus routes and get Prices for those respected routes");
             Console.WriteLine("Enter yo name");
@@ -20,19 +20,12 @@ namespace EndOfYearProject
             Console.WriteLine("Enter your password");
             passWord[2] = Console.ReadLine();
             Console.WriteLine("ryantest");
-            i = 0;
-            if (userName[i] i = 1; i++)
+            
+            if (i = new int userName.Length;)
             {
-                Console.WriteLine("Welcome admin");
-
-
-            else
-
-                {
-                    Console.WriteLine("Welcome user!");
-                }
+                Console.WriteLine($"welcome {userName}");                     
             }
-                Console.WriteLine("test2");
+                Console.WriteLine($"Welcome {userName}");
         }
     }
 }
